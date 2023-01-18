a43c254be836a372f7d04f3b2ed894203a9fdd04
murat-boz <muratboz@hotmail.com>
2023-01-19 00:15:30 +0300

    Add project files.

---
+++ src/ArraysAndHashing/ContainsDuplicate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ContainsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Solution().ContainsDuplicate(new int[] { 1, 2, 3, 4 });
+        }
+    }
+
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> numsHashSet = new HashSet<int>(nums.Length);
+
+            foreach (var num in nums)
+            {
+                if (!numsHashSet.Add(num))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
