cc32f6df789ab75105a791c8ebc6d87b16bdb059
murat-boz <muratboz@hotmail.com>
2023-01-19 22:12:29 +0300

    Added "Valid Anagram" Solution.

---
+++ src/ArraysAndHashing/ValidAnagram/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidAnagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().IsAnagram("anagram", "nagaram"));
+        }
+    }
+
+    public sealed class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s == null || t == null)
+            {
+                return false;
+            }
+
+            string trimmedS = s.Trim();
+            string trimmedT = t.Trim();
+
+            if (trimmedS.Length != trimmedT.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> dictionaryS = this.AddDictionaryWithRule(trimmedS);
+            Dictionary<char, int> dictionaryT = this.AddDictionaryWithRule(trimmedT);
+
+            if (dictionaryS == null || dictionaryT == null)
+            {
+                return false;
+            }
+
+            return !dictionaryS.Except(dictionaryT).Any();
+        }
+
+        private Dictionary<char, int> AddDictionaryWithRule(string value)
+        {
+            var dictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                int bufferValue = 0;
+
+                dictionary.TryGetValue(value[i], out bufferValue);
+
+                if (bufferValue > 0)
+                {
+                    dictionary[value[i]] = bufferValue + 1;
+
+                    continue;
+                }
+
+                dictionary.TryAdd(value[i], bufferValue + 1);
+            }
+
+            return dictionary;
+        }
+    }
+}
