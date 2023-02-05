3b88111921ea9cde7f9d50fad065483806d782b6
murat-boz <muratboz@hotmail.com>
2023-02-05 13:55:31 +0300

    Added "Group Anagrams" Solution.

---
+++ src/ArraysAndHashing/GroupAnagrams/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupAnagrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            string[] array2 = new string[] { "", "" };
+
+            new Solution().GroupAnagrams(array);
+        }
+    }
+
+    public sealed class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var groups = new Dictionary<string, IList<string>>();
+
+            foreach (string s in strs)
+            {
+                char[] hash = new char[26];
+
+                foreach (char c in s)
+                {
+                    hash[c - 'a']++;
+                }
+
+                string key = new string(hash);
+
+                if (!groups.ContainsKey(key))
+                {
+                    groups[key] = new List<string>();
+                }
+
+                groups[key].Add(s);
+            }
+            return groups.Values.ToList();
+        }
+    }
+}
