2c902b1f7453e577fb83487423fd779b98203526
murat-boz <muratboz@hotmail.com>
2023-02-18 14:11:39 +0300

    Added "Longest Consecutive Sequence" Solution.

---
+++ src/ArraysAndHashing/LongestConsecutiveSequence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var test1 = new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+        var test2 = new int[] { 100, 4, 200, 1, 3, 2 };
+        var test3 = new int[] { };
+
+        Console.WriteLine(new Solution().LongestConsecutive(test3));
+    }
+}
+
+public class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int maxLength = 1;
+
+        var hashSets = new HashSet<int>(nums);
+
+        foreach (var hashSet in hashSets)
+        {
+            if (!hashSets.Contains(hashSet - 1))
+            {
+                int valueToLookFor = hashSet;
+
+                while (hashSets.Contains(valueToLookFor + 1))
+                {
+                    valueToLookFor++;
+
+                    maxLength = Math.Max(maxLength, valueToLookFor - hashSet + 1);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
