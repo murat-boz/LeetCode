ce8acd3ad51952ea574ea99b4170cc530b0ba451
murat-boz <muratboz@hotmail.com>
2023-03-02 00:08:54 +0300

    Added Test Of "Valid Palindrome" Solution.

---
+++ test/TwoPointers.Tests/ValidPalindrome.Tests/SolutionTests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+
+namespace ValidPalindrome.Tests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car"                    , false)]
+        public void IsPalindrome(string testValue, bool expected)
+        {
+            //Actual
+            Solution solution = new Solution();
+
+            //Act
+            bool actual = solution.IsPalindrome(testValue);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car"                    , false)]
+        public void IsPalindromeWithFluentAssertion(string testValue, bool expected)
+        {
+            Solution solution = new Solution();
+
+            solution.IsPalindrome(testValue)
+                    .Should()
+                    .Be(expected);
+        }
+    }
+}
