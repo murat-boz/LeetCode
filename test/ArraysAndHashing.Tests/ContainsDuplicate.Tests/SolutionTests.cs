c8b19964b3f05359bac14c67eee619e82099b2b1
murat-boz <muratboz@hotmail.com>
2023-03-04 22:18:02 +0300

    Added Test Of "ContainsDuplicate" Solution.

---
+++ test/ArraysAndHashing.Tests/ContainsDuplicate.Tests/SolutionTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace ContainsDuplicate.Tests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 1 }                  , true)]
+        [InlineData(new int[] { 1, 2, 3, 4 }                  , false)]
+        [InlineData(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        public void ContainsDuplicate(int[] nums, bool expected)
+        {
+            // Act
+            Solution solution = new Solution();
+
+            // Actual
+            var actual = solution.ContainsDuplicate(nums);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 3, 4 }, false)]
+        [InlineData(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        public void ContainsDuplicateWithFluentAssertion(int[] nums, bool expected)
+        {
+            Solution solution = new Solution();
+
+            solution.ContainsDuplicate(nums).Should().Be(expected);
+        }
+    }
+}
