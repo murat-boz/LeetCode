f70f9fa4214551fe687ca3058b116d9f881dede3
murat-boz <muratboz@hotmail.com>
2023-03-05 16:21:40 +0300

    Added Benchmark to "KokoEatingBananas" Solution.

---
+++ src/BinarySearch/KokoEatingBananas/Bency.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace KokoEatingBananas
+{
+    [MemoryDiagnoser(false)]
+    public class Bency
+    {
+        Solution Solution = new Solution();
+
+        [Benchmark]
+        [Arguments(new int[] { 3, 6, 7, 11 }                    , 8)]
+        [Arguments(new int[] { 30, 11, 23, 4, 20 }              , 5)]
+        [Arguments(new int[] { 30, 11, 23, 4, 20 }              , 6)]
+        [Arguments(new int[] { 805306368, 805306368, 805306368 }, 1000000000)]
+        public void MinEatingSpeed(int[] piles, int h)
+        {
+            this.Solution.MinEatingSpeed(piles, h);
+        }
+
+        [Benchmark]
+        [Arguments(new int[] { 3, 6, 7, 11 }                    , 8)]
+        [Arguments(new int[] { 30, 11, 23, 4, 20 }              , 5)]
+        [Arguments(new int[] { 30, 11, 23, 4, 20 }              , 6)]
+        [Arguments(new int[] { 805306368, 805306368, 805306368 }, 1000000000)]
+        public void MinEatingSpeed2(int[] piles, int h)
+        {
+            this.Solution.MinEatingSpeed2(piles, h);
+        }
+
+        [Benchmark]
+        [Arguments(new int[] { 3, 6, 7, 11 }                    , 8)]
+        [Arguments(new int[] { 30, 11, 23, 4, 20 }              , 5)]
+        [Arguments(new int[] { 30, 11, 23, 4, 20 }              , 6)]
+        [Arguments(new int[] { 805306368, 805306368, 805306368 }, 1000000000)]
+        public void MinEatingSpeed3(int[] piles, int h)
+        {
+            this.Solution.MinEatingSpeed3(piles, h);
+        }
+    }
+}
--- src/BinarySearch/KokoEatingBananas/Program.cs
+++ src/BinarySearch/KokoEatingBananas/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace KokoEatingBananas
+﻿using BenchmarkDotNet.Running;
+
+namespace KokoEatingBananas
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            BenchmarkRunner.Run<Bency>();
         }
     }
 
@@ -24,6 +26,68 @@
                 for (int i = 0; i < piles.Length; i++)
                 {
                     midHour = midHour + (int)Math.Ceiling((decimal)piles[i] / midPointer);
+                }
+
+                if (h >= midHour)
+                {
+                    rightPoiner = midPointer - 1;
+
+                    minValue = midPointer;
+                }
+                else
+                {
+                    leftPoiner = midPointer + 1;
+                }
+            }
+
+            return minValue;
+        }
+
+        public int MinEatingSpeed2(int[] piles, int h)
+        {
+            int leftPoiner = 1;
+            int rightPoiner = piles.Max();
+            int minValue = rightPoiner;
+
+            while (leftPoiner <= rightPoiner)
+            {
+                int midPointer = leftPoiner + (rightPoiner - leftPoiner) / 2;
+                long midHour = 0;
+
+                for (int i = 0; i < piles.Length; i++)
+                {
+                    midHour = midHour + (int)Math.Ceiling(piles[i] / (double)midPointer);
+                }
+
+                if (h >= midHour)
+                {
+                    rightPoiner = midPointer - 1;
+
+                    minValue = midPointer;
+                }
+                else
+                {
+                    leftPoiner = midPointer + 1;
+                }
+            }
+
+            return minValue;
+        }
+
+        public int MinEatingSpeed3(int[] piles, int h)
+        {
+            int leftPoiner = 1;
+            int rightPoiner = piles.Max();
+            int minValue = rightPoiner;
+
+            while (leftPoiner <= rightPoiner)
+            {
+                int midPointer = leftPoiner + (rightPoiner - leftPoiner) / 2;
+                long midHour = 0;
+
+                for (int i = 0; i < piles.Length; i++)
+                {
+                    midHour = midHour + (int)Math.Ceiling((double)piles[i] / (double)midPointer);
                 }
 
                 if (h >= midHour)
